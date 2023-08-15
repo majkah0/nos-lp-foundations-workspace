e960a72ec0754605218124b64bf73156b5b2267b
majkah0 <mariahanulova@yahoo.com>
2023-08-16 01:17:37 +0200

    drop na in value

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -41,12 +41,9 @@ def correct_data_types(df: pd.DataFrame) -> pd.DataFrame:
         df['value'], errors = 'coerce')
     return df
 
-def remove_flagged_columns(df: pd.DataFrame) -> pd.DataFrame:
-    return df[df.flag=='']
-
 def export_region_data(df: pd.DataFrame, region: str) -> None:
     df_region = df[df.region == region]
-    df_region = remove_flagged_columns(df_region)
+    df_region=df_region.dropna()
     file_name = region.lower() + '_life_expectancy.csv'
     path = path = Path().cwd() / 'life_expectancy' / 'data' / file_name
     df_region = df_region[['unit', 'sex', 'age', 'year','value']]
