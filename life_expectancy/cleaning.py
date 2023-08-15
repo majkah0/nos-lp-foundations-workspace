905f8789d15daacad7cb85b34d7caa6d43246bc7
majkah0 <mariahanulova@yahoo.com>
2023-08-16 00:39:24 +0200

    it works

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,38 +1,69 @@
-# line length 79                                                              | 
-
 import pandas as pd
+import argparse
+from pathlib import Path
 
-@click.command()
-@click.option('--region', default = 'PT', required=True)
-def clean_data(**kwargs) -> None:
-    """ cleans life expectancy data"""
-    file_path = '/home/maria/projects/nos-lp-foundations-workspace/assignments/life_expectancy/data/eu_life_expectancy_raw.tsv'
-    eu_life_expectancy_df = pd.read_csv(file_path, sep = '\t')
+def import_data(path: str) -> pd.DataFrame:
+    """ reads life expectancy data into a DataFrame"""
+    return pd.read_csv(path, sep = '\t')
+
+def split_first_column(df: pd.DataFrame) -> pd.DataFrame:
+    """ Splits four variables in the first column into their own columns """
+    df[['unit', 'sex', 'age', 'region']] = df[df.columns[0]].str.split(
+                                           ',', expand=True)
+    df = df.drop(columns = df.columns[0])
+    return df
+
+def remove_spaces_from_column_names(df: pd.DataFrame) -> pd.DataFrame:
+    """ Removes extra space from column names."""   
+    df = df.rename(str.strip, axis = 'columns')
+    return df
     
-    eu_life_expectancy_df[['unit', 'sex', 'age', 'region']] = eu_life_expectancy_df[
-        eu_life_expectancy_df.columns[0]].str.split(',', expand=True)
-    eu_life_expectancy_df = eu_life_expectancy_df.drop(
-        columns = eu_life_expectancy_df.columns[0])
-    
-    eu_life_expectancy_df = eu_life_expectancy_df.rename(str.strip, axis = 'columns')
-    
-    years = eu_life_expectancy_df.columns[:-4]
-    eu_life_expectancy_df_long = pd.melt(
-        eu_life_expectancy_df, id_vars = ['unit', 'sex', 'age', 'region'], 
-        value_vars = years, var_name = 'year', value_name = 'value')
-    
-    for col in eu_life_expectancy_df_long.columns[:4]:
-        eu_life_expectancy_df_long[col] = eu_life_expectancy_df_long[col].astype('string')
-    eu_life_expectancy_df_long['year'] = eu_life_expectancy_df_long['year'].astype('int')
-    eu_life_expectancy_df_long['value'] = pd.to_numeric(
-        eu_life_expectancy_df_long['value'], errors = 'coerce')
-    
-    pt_life_expectancy = eu_life_expectancy_df_long[
-        eu_life_expectancy_df_long.region == region].copy()
-    file_path = '/home/maria/projects/nos-lp-foundations-workspace/assignments/life_expectancy/data/pt_life_expectancy.csv'
-    pt_life_expectancy.to_csv(file_path, index = False)
+def melt_df(df: pd.DataFrame) -> pd.DataFrame:
+    """ Melts DataFrame. """
+    years = df.columns[:-4]
+    return pd.melt(
+        df, 
+        id_vars = ['unit', 'sex', 'age', 'region'], 
+        value_vars = years, var_name = 'year', 
+        value_name = 'value')
+
+def extract_flag(df: pd.DataFrame) -> pd.DataFrame:
+    """Extract flag (b, e, p or their combination)
+    from the value column."""
+    df[['value','flag']] = df['value'].str.split(' ', expand=True)
+    return df
+
+def correct_data_types(df: pd.DataFrame) -> pd.DataFrame:
+    for col in ['unit', 'sex', 'age', 'region','flag']:
+        df[col] = df[col].astype('string')
+    df['year'] = df['year'].astype('int')
+    df['value'] = pd.to_numeric(
+        df['value'], errors = 'coerce')
+    return df
+
+def export_region_data(df: pd.DataFrame, region: str) -> None:
+    df_region = df[df.region == region][['unit', 'sex', 'age', 'year','value']].copy()
+    file_name = region.lower() + '_life_expectancy.csv'
+    path = path = Path().cwd() / 'life_expectancy' / 'data' / file_name
+    df_region.to_csv(path, index = False)    
+
+def clean_data(region: str = 'PT') -> None:
+    """ 
+    imports and cleans life expectancy data
+    exports data for the chosen region         
+    """
+    path = Path().cwd() / 'life_expectancy' / 'data' / 'eu_life_expectancy_raw.tsv'
+    eu_life_expectancy_df = import_data(path)
+    eu_life_expectancy_df = split_first_column(eu_life_expectancy_df)
+    eu_life_expectancy_df = remove_spaces_from_column_names(eu_life_expectancy_df)
+    eu_life_expectancy_df = melt_df(eu_life_expectancy_df)
+    eu_life_expectancy_df = extract_flag(eu_life_expectancy_df)
+    eu_life_expectancy_df = correct_data_types(eu_life_expectancy_df)
+    export_region_data(eu_life_expectancy_df, region)
 
 if __name__ == "__main__":  # pragma: no cover
-    clean_data(region = 'PT')    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("region", help = 'Input region argument.', type = str)
+    args = parser.parse_args()
+    clean_data(region = args.region)
     
-
