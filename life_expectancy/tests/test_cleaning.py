419e51d67a9ccf5cf5b58b97a66ab8c861f8783c
majkah0 <mariahanulova@yahoo.com>
2023-08-22 18:58:32 +0200

    adjust tests to refactored code

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,13 +1,16 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import load_data, clean_data, save_data
 from . import OUTPUT_DIR
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data(region = 'PT')
+    region = 'PT'
+    eu_life_expectancy_data = load_data()
+    region_life_expectancy_data = clean_data(eu_life_expectancy_data, region = region)
+    save_data(region_life_expectancy_data, region)
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
