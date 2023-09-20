b6996e5806e1ebc5be76292641fe99afe1b3fe1a
majkah0 <mariahanulova@yahoo.com>
2023-09-20 17:34:46 +0200

    Add unit tests

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,17 +1,27 @@
-"""Tests for the cleaning module"""
-import pandas as pd
+"""Tests for all modules"""
 
-from life_expectancy.cleaning import main
+import pandas as pd
+from load_save import load_data, save_data
+from cleaning import clean_data
+from life_expectancy.main import main
+from unittest.mock import Mock, patch
 from . import OUTPUT_DIR
 
+def test_clean_data() -> pd.DataFrame:
+    pd.testing.assert_frame_equal(clean_data(pt_life_expectancy_input, 'PT'),
+                                  pt_life_expectancy_expected)
+    
+@patch("load_save.pd.DataFrame.to_csv")
+def test_save_data(to_csv_mock: Mock):
+    to_csv_mock(side_effect=print('Saved data'))
+    save_data(pt_life_expectancy_expected, 'PT')
+    to_csv_mock.assert_called_once()
 
-def test_clean_data(pt_life_expectancy_expected):
+def test_main(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
     region = 'PT'
-    main(region = region)
-    pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
-    )
+    #how to put the fixture?
+    pt_life_expectancy_actual = main(region = region)
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
     )
