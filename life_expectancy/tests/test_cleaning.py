789a3dfde6896f8e522c0315a9f6bf376903b9fd
majkah0 <mariahanulova@yahoo.com>
2023-09-22 17:28:28 +0200

    Correct fixtures

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -4,24 +4,43 @@ import pandas as pd
 from ..load_save import load_data, save_data
 from ..cleaning import clean_data
 from ..main import main
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, patch
 from . import OUTPUT_DIR, FIXTURES_DIR
 
-def test_clean_data(pt_life_expectancy_input, pt_life_expectancy_expected) -> pd.DataFrame:
-    pd.testing.assert_frame_equal(clean_data(pt_life_expectancy_input, 'PT'),
-                                  pt_life_expectancy_expected)
+def _correct_data_types(df: pd.DataFrame) -> pd.DataFrame:
+    """Set correct data types.
     
-@patch("load_save.pd.DataFrame.to_csv")
-def test_save_data(to_csv_mock: Mock, pt_life_expectancy_expected):
-    to_csv_mock(side_effect=print('Saved data'))
-    save_data(pt_life_expectancy_expected, 'PT')
-    to_csv_mock.assert_called_once()
+    Args:
+        df (pd.DataFrame): The data.
 
-def test_main(pt_life_expectancy_expected):
+    Returns:
+        df (pd.DataFrame): The modified data.
+    """
+    for col in ['unit', 'sex', 'age', 'region']:
+        df[col] = df[col].astype('string')
+    df['year'] = df['year'].astype('int')
+    df['value'] = pd.to_numeric(
+        df['value'])
+    return df
+
+def test_clean_data(pt_life_expectancy_input, pt_life_expectancy_cleaned_expected) -> pd.DataFrame:
+    pt_life_expectancy_cleaned_actual = clean_data(pt_life_expectancy_input, 'PT')
+    pd.testing.assert_frame_equal(pt_life_expectancy_cleaned_actual,
+                                  _correct_data_types(pt_life_expectancy_cleaned_expected))
+
+def test_save_data(pt_life_expectancy_cleaned_expected):
+    to_csv_mock = MagicMock()
+    with patch("./load_save.save_data.pd.DataFrame.to_csv", to_csv_mock):
+         to_csv_mock(side_effect=print('Saved data'))
+         save_data(pt_life_expectancy_cleaned_expected, 'PT')
+         to_csv_mock.assert_called_once()
+
+def test_main():
     """Run the `clean_data` function and compare the output to the expected output"""
     region = 'PT'
-    #how to put the fixture?
-    pt_life_expectancy_actual = main(region = region)
+    df_actual = main(region = region)
+    df_expected = pd.read_csv(
+        FIXTURES_DIR / 'pt_life_expectancy_total_expected.csv',index_col=[0])
+    df_expected = _correct_data_types(df_expected)
     pd.testing.assert_frame_equal(
-        pt_life_expectancy_actual, pt_life_expectancy_expected
-    )
+        df_actual, df_expected)
