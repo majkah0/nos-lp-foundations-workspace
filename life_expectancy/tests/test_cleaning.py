c77019e67a64295d8b3e9a873146d4aecf6c867d
majkah0 <mariahanulova@yahoo.com>
2023-10-16 16:47:45 +0200

    Fix double call to mock

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -50,7 +50,7 @@ def test_save_data(to_csv_mock: Mock, pt_life_expectancy_cleaned_expected: pd.Da
     """
     def _print_message(*args, **kwargs):
         print('Saved file')
-    to_csv_mock(side_effect= _print_message)
+    to_csv_mock.side_effect= _print_message
     save_data(pt_life_expectancy_cleaned_expected, 'PT')
     expected_path = BASE_DIR / 'pt_life_expectancy.csv'
     to_csv_mock.assert_called_once_with(expected_path, index=False)
